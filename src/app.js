138aaf42e78408389608af7206fe5537b51ec4b0
nermindonat <nermingurses21@gmail.com>
2021-10-19 12:09:50 +0300

    18 Ekim Ders Sonu

---
+++ src/app.js
@@ -0,0 +1,29 @@
+console.log("Merhaba Kodlama.io")
+
+
+
+let dolarBugun=9.30
+let  dolarDun=9.20
+dolarDun="9.20"
+
+{
+    let dolarDun=9.10
+}
+console.log(dolarDun)
+
+const euroDun=11.2
+euroDun=11
+
+console.log(euroDun)
+//CamelCasing
+let konutKredileri=["Konut Kredisi","Emlak Lonut Kredisi","Kamu Konut Kredisi","Özel Lonut Kredisi"]
+
+console.log("<ul>")
+for (let i = 0; i<konutKredileri.length; i++) {
+    // const element = array[index];
+
+    console.log("<li>"+konutKredileri[i]+"</li>")
+    
+}
+console.log("</ul>")
+
